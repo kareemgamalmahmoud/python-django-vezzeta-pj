45744545434e8428e09f6d6a1c02470d51726a22
kareemgamalmahmoud <kareem.shehata1999@gmail.com>
2020-09-06 16:23:33 +0200

    some data

--- accounts/models.py
+++ accounts/models.py
@@ -9,6 +9,7 @@ class Profile(models.Model):
     name = models.CharField(_("الاسم : "),max_length=50)
     who_i = models.TextField(_("من انا : "),max_length=250)
     price = models.IntegerField(_("سعر الكشف : "))
+    image = models.ImageField(_("صورة شخصية :"),upload_to='profile')
 
     class Meta:
         pass
